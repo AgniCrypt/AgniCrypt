4e92ea3e6185a1249c7ccd44fe91782256d1a401
Jay Aditya <74151731+MaiHuJay@users.noreply.github.com>
2022-02-03 12:13:25 +0530

    Update Real_time_graph_plotter.py

--- Real_time_graph_plotter.py
+++ Real_time_graph_plotter.py
@@ -1,10 +1,12 @@
 import cryptocompare
 
+stock = 'BTC'
+
 #Get price of crypto in USD
-#print(cryptocompare.get_price('BTC',currency='USD')['BTC']['USD'])
+#print(cryptocompare.get_price(stock,currency='USD')[stock]['USD'])
 
 #Get full name of cryptocurrency
-#cryptocompare.get_coin_list()['BTC']['FullName']
+#cryptocompare.get_coin_list()[stock]['FullName']
 
 def get_crypto_price(cryptocurrency,currency):
     return cryptocompare.get_price(cryptocurrency, currency=currency)[cryptocurrency][currency]
@@ -12,8 +14,8 @@ def get_crypto_price(cryptocurrency,currency):
 def get_crypto_name(cryptocurrency):
     return cryptocompare.get_coin_list()[cryptocurrency]['FullName']
 
-#print(get_crypto_price('BTC','USD'))
-#print(get_crypto_name('BTC'))
+#print(get_crypto_price(stock,'USD'))
+#print(get_crypto_name(stock))
 
 import matplotlib.pyplot as plt
 from datetime import datetime
@@ -27,10 +29,10 @@ y_vals = []
 
 def animate(i):
     x_vals.append(datetime.now())
-    y_vals.append(get_crypto_price('BTC','USD'))
+    y_vals.append(get_crypto_price(stock,'USD'))
 
     plt.cla()
-    plt.title(get_crypto_name('BTC')+'Price Live Plotting')
+    plt.title(get_crypto_name(stock)+'Price Live Plotting')
     plt.xlabel('Date')
     plt.ylabel('Price(USD)')
     
